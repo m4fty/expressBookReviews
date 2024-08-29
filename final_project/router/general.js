f0aae6fd43d673058ee0fce8892afe3fb4f38c7f
m4fty <jaime.ferreira@99minutos.com>
2024-08-29 16:18:12 -0400

    Item 3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -27,10 +27,16 @@ public_users.get("/isbn/:isbn", function (req, res) {
 
 // Get book details based on author
 public_users.get("/author/:author", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const author = req.params.author.toLowerCase();
+  const booksAux = Object.values(books).filter(
+    (book) => book.author.toLowerCase() === author
+  );
+  if (booksAux.length > 0) {
+    return res.status(300).send(JSON.stringify(booksAux, null, 4));
+  } else {
+    return res.status(404).json({ message: "No books found by author" });
+  }
 });
-
 // Get all books based on title
 public_users.get("/title/:title", function (req, res) {
   //Write your code here
