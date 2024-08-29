81887090a528751363d1855e4ebdcccc6352469f
m4fty <jaime.ferreira@99minutos.com>
2024-08-29 17:05:27 -0400

    Task 6

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,8 +5,20 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 public_users.post("/register", (req, res) => {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  console.log(req.body);
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res
+      .status(400)
+      .json({ message: "Username and password are required" });
+  }
+  if (isValid(username)) {
+    return res.status(409).json({ message: "Username already exists!" });
+  }
+  users.push({ username, password });
+  return res
+    .status(200)
+    .json({ message: "User successfully registered. Now you can login" });
 });
 
 // Get the book list available in the shop
@@ -58,9 +70,9 @@ public_users.get("/review/:isbn", function (req, res) {
   const isbn = req.params.isbn;
   const book = books[isbn];
   if (book) {
-      return res.status(300).send(JSON.stringify(book.reviews, null, 4));
+    return res.status(300).send(JSON.stringify(book.reviews, null, 4));
   } else {
-      return res.status(404).json({ message: "No book found" });
+    return res.status(404).json({ message: "No book found" });
   }
 });
 
