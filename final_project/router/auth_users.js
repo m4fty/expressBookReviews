6b474e676d4a8de765dcecded97101e1770a8d82
m4fty <jaime.ferreira@99minutos.com>
2024-08-29 17:44:09 -0400

    Task 9

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -66,6 +66,25 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
   });
 });
 
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+  if (!req.session.authorization) {
+    return res.status(400).json({ message: "User not authenticated" });
+  }
+  const username = req.session.authorization.username;
+  if (!books[isbn]) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+  if (books[isbn].reviews && books[isbn].reviews[username]) {
+    delete books[isbn].reviews[username];
+    return res
+      .status(200)
+      .json({ message: "Review deleted", reviews: books[isbn].reviews });
+  } else {
+    return res.status(404).json({ message: "No review found" });
+  }
+});
+
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
 module.exports.users = users;
